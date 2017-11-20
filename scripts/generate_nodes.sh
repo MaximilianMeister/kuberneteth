e713676835e1d1724a824a47c7a966a25981041a
Maximilian Meister <mmeister@suse.de>
2017-11-20 21:33:55 +0100

    adapt node generation script
    Signed-off-by: Maximilian Meister <mmeister@suse.de>

--- scripts/generate_nodes.sh
+++ scripts/generate_nodes.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
-if [[ $1 =~ ^(-h|--help)$ ]] || [ -z "$1" ] || [ -z "$2" ] ; then
+if [[ $1 =~ ^(-h|--help)$ ]] || [ -z "$1" ] ; then
   cat <<EOF
 usage:
-  ./generate_nodes.sh n..range [etherbase]
+  ./generate_nodes.sh nodenumber [etherbase]
 example:
-  ./generate_nodes.sh 0 100 0x023e291a99d21c944a871adcc44561a58f99bdbc
+  ./generate_nodes.sh 100 0x023e291a99d21c944a871adcc44561a58f99bdbc
 EOF
   exit 1
 fi
 
-if [ -z "$3" ]; then
+if [ -z "$2" ]; then
   ETHERBASE="0x023e291a99d21c944a871adcc44561a58f99bdbc"
 else
-  ETHERBASE=$3
+  ETHERBASE=$2
 fi
 
-for i in $(seq $1 $2); do
+for i in $(seq 0 $1); do
+name=n$(hexdump -n 8 -e '2/4 "%08X" 1 "\n"' /dev/random | awk '{print tolower($0)}')
 cat <<EOF
-- miner$i:
+- $name:
     k8s:
       replicas: 1
     geth:
       Eth_Etherbase: "$ETHERBASE"
       Eth_MinerThreads: 1
-      Node_UserIdent: miner$i
-      Node_DataDir: /etc/testnet/miner$i
+      Node_UserIdent: $name
+      Node_DataDir: /etc/testnet/$name
       Node_HTTPPort: 8545
       Node_WSPort: 8546
       NodeP2P_ListenAddr: 30303
